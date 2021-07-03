6eeacd3cda35c94cdd657385d354558f708b94f8
Keerthana <kp79@njit.edu>
2021-07-04 01:08:57 +0530

    Feature: Set up calculator

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -4,6 +4,10 @@ from Calculator import Calculator
 
 
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
     def test_instantiate_calculator(self):
         calculator = Calculator()
         self.assertIsInstance(calculator,Calculator)
