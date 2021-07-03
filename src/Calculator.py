bfd76338c4e702ed14f5f6d76beec76a9a7f4f18
Keerthana <kp79@njit.edu>
2021-07-04 00:52:27 +0530

    Feature: Set up calculator with Addition and Subtraction

--- src/Calculator.py
+++ src/Calculator.py
@@ -6,6 +6,12 @@ def addition(a, b):
     c = a + b
     return c
 
+def subtraction(a, b):
+    a = int(a)
+    b = int(b)
+    c = b - a
+    return c
+
 
 class Calculator:
     result = 0
@@ -17,4 +23,8 @@ class Calculator:
 
     def add(self, a, b):
         self.result = addition(a, b)
+        return self.result
+
+    def sub(self,a,b):
+        self.result = subtraction(a,b)
         return self.result
