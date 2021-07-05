e33216be814c18f1a573858a6b48d9d7859a5979
Keerthana <kp79@njit.edu>
2021-07-06 00:55:40 +0530

    Task: Added Subtraction Method

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -19,6 +19,12 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
+    def test_sub_method_calculator(self):
+        test_data = CsvReader("src/UnitTestSubtraction.csv").data
+        for row in test_data:
+            self.assertEqual(self.calculator.sub(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
 
 
 
